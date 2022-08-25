b5143e2d060cde561a5b90928735618562ad2781
Ricardo Silva <ricardosilva.rss@gmail.com>
2022-08-25 23:18:52 +0100

    added 1 to many relation

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ApplicationRecord
+  has_many :tasks
+
   validates :name, presence: true
   validates :description, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  belongs_to :list
+
   validates :title, presence: true
   validates :description, presence: true
 end
